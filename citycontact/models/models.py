f39d59d3e41ca1f6b64e1d8a31acbb6492f9c7b7
Bilel <bilel@Macbook.local>
2021-07-15 17:01:41 +0100

    modif company

--- citycontact/models/models.py
+++ citycontact/models/models.py
@@ -9,10 +9,6 @@ class Partner(models.Model):
 
     
     city = fields.Many2one('res.city', string='City')
-    dzcompany_type = fields.Selection(
-        [('Sarl', 'SARL'), ('Eurl', 'EURL'), ('Spa', 'SPA'), ('Snc', 'SNC')
-         
-         ], string='Forme juridique ')
 
 
     
