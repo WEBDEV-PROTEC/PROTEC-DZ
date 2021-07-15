709cd0acb6d88e03dad78d8068d846efacbbffd4
Bilel <bilel@Macbook.local>
2021-07-15 16:45:21 +0100

    modif company

---
+++ dzcompany/models/models.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import Warning, UserError, ValidationError
+import pdb
+import requests
+import json
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    company_type = fields.Selection(
+        [('Sarl', 'SARL'), ('Eurl', 'EURL'), ('Spa', 'SPA'), ('Snc', 'SNC')
+         
+         ], string='Forme juridique ')
+
