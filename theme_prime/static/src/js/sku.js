9dd251a19be369b9ed33cc64b9f0a428fa6aa1f0
Protec <protec@iMac-de-Protec.local>
2021-11-04 16:59:11 +0100

    ref variant

--- theme_prime/static/src/js/sku.js
+++ theme_prime/static/src/js/sku.js
@@ -1,13 +1,26 @@
 $( document ).ready(function() {
-    $('#sku').html($('input:radio.always[checked="checked"]').attr('code'))
+    if ($('input:radio.always[checked="checked"]').length>0) {$('#al').html($('input:radio.always[checked="checked"]').attr('code'))}
+    if ($('select.always"]').length>0) {setTimeout(() => {$('#al').html($('select.always > option[selected="selected"]').attr('code'))}, 300);}
+    
+    
 });
 
 $('input:radio.always').change(
     function(){
         if ($(this).is(':checked') )
-            $('#sku').html($(this).attr('code'))
+            $('#al').html($(this).attr('code'))
             console.log("lol")
         }
     );
 
 
+
+    
+    $('select.always').change(
+        function(){
+          
+                  setTimeout(() => {$('#al').html($('select.always > option[selected="selected"]').attr('code'))}, 100);
+            }
+        );
+    
+    
