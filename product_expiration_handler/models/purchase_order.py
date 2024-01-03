c935be9993429cc1a030c255d0b42bb94b7ed476
djamelhemch <mohameddjamel.hemch.dz@viacesi.fr>
2024-01-03 09:44:22 +0100

    migration to v16 commit
    add modules

--- product_expiration_handler/models/purchase_order.py
+++ product_expiration_handler/models/purchase_order.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+<<<<<<< HEAD
 import logging
 from datetime import datetime, timedelta
 
@@ -13,16 +14,33 @@ class PurchaseOrderLine(models.Model):
         help='Link to the product template',
     )
 
+=======
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+    product_template_id = fields.Many2one(
+    'product.template',
+    string='Product Template',
+    help='Link to the product template',
+)
+>>>>>>> 2a6c377 (migration to v16 commit)
     purchased_expiration_time = fields.Selection([
         ('1_month', '1 Month'),
         ('3_months', '3 Months'),
         ('6_months', '6 Months'),
         ('9_months', '9 Months'),
         ('1_year', '1 Year'),
+<<<<<<< HEAD
         ('3_years', '3 Year'),
         ('5_years', '5 Years'),
         ('10_years', '10 Years'),
     ], string='Expiration Time', default='9_months', help='Select the purchased expiration period')
+=======
+        ('3_year', '3 Year'),
+        ('5_years', '5 Years'),
+        ('10_years', '10 Years'),
+    ], string='Expiration Time', help='Select the purchased expiration period')
+>>>>>>> 2a6c377 (migration to v16 commit)
 
     @api.model
     def create(self, values):
@@ -38,6 +56,7 @@ class PurchaseOrderLine(models.Model):
     def enable_tracking(self):
         for line in self:
             if line.purchased_expiration_time:
+<<<<<<< HEAD
                 _logger.info(f"Enabling tracking for product: {line.product_id.name}")
 
                 # Enable tracking for the related product
@@ -46,6 +65,14 @@ class PurchaseOrderLine(models.Model):
                 # Set use_expiration_date to true
                 line.product_id.product_tmpl_id.write({'use_expiration_date': True})
 
+=======
+                # Enable tracking for the related product
+                line.product_id.product_tmpl_id.write({'tracking': 'serial'})
+                
+                # Set use_expiration_date to true
+                line.product_id.product_tmpl_id.write({'use_expiration_date': True})
+                
+>>>>>>> 2a6c377 (migration to v16 commit)
                 # Set expiration_time based on the selected purchased_expiration_time
                 expiration_times = {
                     '1_month': 30,
@@ -57,6 +84,7 @@ class PurchaseOrderLine(models.Model):
                     '5_years': 5 * 365,
                     '10_years': 10 * 365,
                 }
+<<<<<<< HEAD
                 expiration_time = expiration_times.get(line.purchased_expiration_time, 0)
                 _logger.info(f"Setting expiration_time to: {expiration_time}")
                 line.product_id.product_tmpl_id.write({'expiration_time': expiration_time})
@@ -80,3 +108,10 @@ class PurchaseOrderLine(models.Model):
             alert_date = today_date + timedelta(days=90)
             _logger.info(f"Setting 'cartouche d'encre' alert_time to: {alert_date}")
             line.product_id.product_tmpl_id.write({'alert_time': (alert_date - today_date).days})
+=======
+                line.product_id.product_tmpl_id.write({'expiration_time': expiration_times.get(line.purchased_expiration_time, 0)})
+                
+                # Set alert_time to expiration_time - 15
+                alert_time = expiration_times.get(line.purchased_expiration_time, 0) - 15
+                line.product_id.product_tmpl_id.write({'alert_time': alert_time})
+>>>>>>> 2a6c377 (migration to v16 commit)
