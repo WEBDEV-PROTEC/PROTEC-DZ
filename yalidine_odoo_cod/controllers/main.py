fe16c38f4fecd820818395fb7b6dcbf737320f7b
djamelhemch <mohameddjamel.hemch.dz@viacesi.fr>
2024-05-09 09:17:04 +0100

    module updates and logging modifications

--- yalidine_odoo_cod/controllers/main.py
+++ yalidine_odoo_cod/controllers/main.py
@@ -1,6 +1,7 @@
 import logging
 from odoo import http
 from odoo.http import request
+import requests
 
 _logger = logging.getLogger(__name__)
 
@@ -8,6 +9,8 @@ class YalidineAPIController(http.Controller):
 
     @http.route('/shop/delivery', type='http', auth='public', website=True)
     def index(self):
+        _logger.info("Received request to display delivery information")
+
         # Get the current order ID from the session
         order_id = request.session.get('sale_order_id')
         name = request.session.get('name')
@@ -20,7 +23,6 @@ class YalidineAPIController(http.Controller):
         ]
 
         _logger.info("Received request to display delivery information for order ID: %s", order_id)
-        # Log the delivery address information
         _logger.info("Delivery address requested from session variable: %s", delivery_address)
 
         return request.render('yalidine_odoo_cod.delivery_histories', {
@@ -29,12 +31,15 @@ class YalidineAPIController(http.Controller):
         
     @http.route('/shop/confirmation', type='http', auth='public', website=True)
     def process_payment(self, **kwargs):
+        _logger.info("Starting payment process")
+
         # Retrieve the order ID from the session
         order_id = request.session.get('sale_order_id')
         if not order_id:
             _logger.warning("Order ID not found in session")
             return "Order ID not found in session"
 
+        _logger.info("Retrieving order with ID: %s", order_id)
         # Retrieve the order
         order = request.env['sale.order'].sudo().browse(order_id)
 
@@ -89,6 +94,7 @@ class YalidineAPIController(http.Controller):
         }
         _logger.info("Created yalidine payload: %s", payload)
         try:
+            _logger.info("Making API call to create parcel")
             response = requests.post(url, json=payload, headers=headers)
             response.raise_for_status()
             _logger.info("Parcel created successfully: %s", response.json())
@@ -98,78 +104,27 @@ class YalidineAPIController(http.Controller):
             return "Error creating parcel: %s" % e
 
     def calculate_total_amount_weight_and_shipping_cost(self, order_id, city):
+        _logger.info("Calculating total amount, weight, and shipping cost")
         order = request.env['sale.order'].sudo().browse(order_id)
         items_value = sum(order.order_line.mapped('price_total'))
         total_weight = sum(order.order_line.mapped('product_id.weight'))
         product_names = [line.product_id.name for line in order.order_line]
-        _logger.info("Calculating total amount and weight")
+
+        _logger.info("Items value: %s, Total weight: %s, Product names: %s", items_value, total_weight, product_names)
         # Here you can calculate shipping cost and total amount based on your business logic
         shipping_cost = self.calculate_shipping_cost(city)  # Replace with actual shipping cost calculation
         total_amount = shipping_cost + items_value
 
         return items_value, total_weight, product_names, shipping_cost, total_amount
 
-    def get_city_id(city_name):
-        # Dictionary mapping city names to their corresponding ID
-        city_ids = {
-            'Adrar': 1,
-            'Chlef': 2,
-            'Laghouat': 3,
-            'Oum El Bouaghi': 4,
-            'Batna': 5,
-            'Bejaïa': 6,
-            'Biskra': 7,
-            'Béchar': 8,
-            'Blida': 9,
-            'Bouira': 10,
-            'Tamanrasset': 11,
-            'Tebessa': 12,
-            'Tlemcen': 13,
-            'Tiaret': 14,
-            'Tizi Ouzou': 15,
-            'Alger': 16,
-            'Djelfa': 17,
-            'Djijel': 18,
-            'Sétif': 19,
-            'Saïda': 20,
-            'Skikda': 21,
-            'Sidi Bel Abbès': 22,
-            'Annaba': 23,
-            'Guelma': 24,
-            'Constantine': 25,
-            'Médéa': 26,
-            'Mostaganem': 27,
-            "M'Sila": 28,
-            'Mascara': 29,
-            'Ouargla': 30,
-            'Oran': 31,
-            'El Bayadh': 32,
-            'Illizi': 33,
-            'Bordj Bou Arreridj': 34,
-            'Boumerdès': 35,
-            'El Tarf': 36,
-            'Tindouf': 37,
-            'Tissemsilt': 38,
-            'El Oued': 39,
-            'Khenchela': 40,
-            'Souk Ahras': 41,
-            'Tipaza': 42,
-            'Mila': 43,
-            'Aïn Defla': 44,
-            'Naâma': 45,
-            'Aïn Témouchent': 46,
-            'Ghardaia': 47,
-            'Relizane': 48
-        }
-    
-        return city_ids.get(city_name, 0)  # Return 0 if city not found
-    
-    def calculate_shipping_cost(city_id):
+    def calculate_shipping_cost(self, city):
+        _logger.info("Calculating shipping cost for city: %s", city)
+        city_id = self.get_city_id(city)
         if city_id == 0:
-            print('city id is 0')
+            _logger.warning("City ID not found for city: %s", city)
             return 0  # City not found, shipping cost is 0
-        
-        _logger.info("Calculating total amount and weight")
+
+        _logger.info("Fetching shipping cost from Yalidine API for city ID: %s", city_id)
         # Make API call to get shipping cost
         api_url = f'https://api.yalidine.app/v1/deliveryfees/?wilaya_id={city_id}'
         api_id = '30766545130987580386'
@@ -178,7 +133,7 @@ class YalidineAPIController(http.Controller):
             'X-API-ID': api_id,
             'X-API-TOKEN': api_token
         }
-    
+
         response = requests.get(api_url, headers=headers)
         if response.status_code == 200:
             data = response.json().get('data', [])
@@ -186,10 +141,10 @@ class YalidineAPIController(http.Controller):
                 if item.get('wilaya_id') == city_id:
                     home_fee = item.get('home_fee', 0)
                     shipping_cost = home_fee
+                    _logger.info("Shipping cost retrieved successfully: %s", shipping_cost)
                     return shipping_cost
-            print(f'shipping costs from api {shipping_cost}')
         else:
-            print("failed api call")
+            _logger.error("Failed to retrieve shipping cost from API")
             shipping_cost = 0  # Default to 0 if API call fails
-    
+
         return shipping_cost
