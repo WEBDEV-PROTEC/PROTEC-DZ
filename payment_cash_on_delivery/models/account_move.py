b5e76f3229e632135b5faa37375e06e6d221d784
djamelhemch <mohameddjamel.hemch.dz@viacesi.fr>
2024-05-12 09:08:44 +0100

    fix multiple invoice payment error

--- payment_cash_on_delivery/models/account_move.py
+++ payment_cash_on_delivery/models/account_move.py
@@ -7,12 +7,19 @@ class CodAccountMove(models.Model):
     _inherit = "account.move"
 
     def action_invoice_paid(self):
-        if self and self.payment_state == "paid":
-            try:
-                payment_transections = self.env["payment.transaction"].search([("state",'=','pending'),("provider", "=", "cash_on_delivery")])
-                cod_transection = payment_transections and payment_transections.filtered(lambda txn: self.id in txn.invoice_ids.ids)
-                txn = cod_transection and cod_transection[0]
-                if txn:
-                    txn.state = "done"
-            except Exception as e:
-                _logger.info("\n\n\n[COD]: EXCEPTION in auto done payment transection....%r\n\n\n",str(e))
+        # Loop through each invoice in self to handle multiple invoices safely
+        for record in self:
+            if record.payment_state == "paid":
+                try:
+                    # Search for pending transactions for cash on delivery
+                    payment_transactions = self.env["payment.transaction"].search([
+                        ("state", "=", "pending"),
+                        ("provider", "=", "cash_on_delivery"),
+                        ("invoice_ids", "in", record.id)  # Ensure the transaction is related to the current invoice
+                    ])
+                    if payment_transactions:
+                        # Assuming only one transaction per invoice, but if multiple, consider handling or logging
+                        for txn in payment_transactions:
+                            txn.state = "done"
+                except Exception as e:
+                    _logger.info("\n\n\n[COD]: EXCEPTION in auto done payment transaction....%r\n\n\n", str(e))
