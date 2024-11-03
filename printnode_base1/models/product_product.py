2010e21e5492f8f0b6a4b7200ff54272617414ad
Abdessamad <abdessamadbm31@gmail.com>
2024-11-03 11:35:53 +0100

    Delete print node 14

--- printnode_base1/models/product_product.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2021 VentorTech OU
-# See LICENSE file for full copyright and licensing details.
-
-from odoo import models
-
-
-class ProductProduct(models.Model):
-    _name = 'product.product'
-    _inherit = ['product.product', 'multi.print.mixin']
