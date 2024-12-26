647bd886696b35c2fb764492f964de56ed6c6386
neha453 <neha.gupta453@webkul.in>
2024-12-26 13:00:30 +0530

    FIX script

--- website_webkul_addons/migrations/17.0.1.0.1/pre-migrate.py
+++ website_webkul_addons/migrations/17.0.1.0.1/pre-migrate.py
@@ -27,6 +27,55 @@ def migrate(cr, version):
     cr.execute("""
                 delete from ir_model_data where module in ('abs_so_minimum_quantity', 'al_web_price_hide', 'amount_in_letters', 'auto_generate_product_code_app', 'batch_payment_pdf',  'contact_protec', 'custom_login', 'custom_pricelist_report', 'customer_sale_history', 'droggol_mass_mailing_themes', 'emipro_theme_banner_video', 'emipro_theme_base', 'emipro_theme_brand', 'emipro_theme_category_listing', 'emipro_theme_landing_page', 'emipro_theme_lazy_load', 'emipro_theme_load_more', 'emipro_theme_product_carousel', 'emipro_theme_product_label', 'emipro_theme_product_label_extended', 'emipro_theme_product_tabs', 'emipro_theme_product_timer', 'emipro_theme_quick_filter', 'fl_so_po_multi_products', 'images_order',  'l10n_dz_elosys', 'multi_warehouse_sale_order', 'payment_cash_on_delivery', 'printnode_base', 'product_banner_a6', 'product_catalogue', 'product_price_slots', 'protec_optional_products', 'protec_partners', 'pwa_ept', 'sales_credit_limit', 'sh_call_for_price', 'sh_shop_qty', 'sh_website_category_page', 'signupavinet', 'stock_filter', 'stock_notifier', 'tags', 'tags_product', 'theme_clarico_vega', 'timbre_fiscal', 'video_player', 'webdescription', 'website_product_pack', 'website_stock', 'website_stock_notifiy', 'wk_product_pack'); 
                """)
+    cr.execute("""delete from ir_asset;
+                delete from ir_cron where id=35;
+                delete from ir_cron where id=44;
+                delete from ir_cron where id=61;
+               delete from ir_cron where id=59;
+               delete from ir_cron where id=60;
+                delete from ir_model where model='website.stock.config.settings';
+                delete from  website_notifiy_config_settings;
+                delete from mail_template where model='website.stock.notify';
+                delete from ir_model where model='config.timbre';
+                delete from ir_model where model='product.brand.ept';
+                delete from ir_model where model='approval.signup';
+                delete from ir_model where model='sale.call.for.price';
+                delete from ir_model where model='website.stock.notify';
+                delete from ir_model where model='printnode.report.policy';
+                delete from ir_model where model='printnode.printer';
+                delete from ir_model where model='printnode.action.button';
+                delete from ir_model where model='printnode.scenario';
+                delete from ir_model where model='printnode.rule';
+                delete from ir_model where model='shipping.label';
+                delete from ir_model where model='printnode.printjob';
+                delete from ir_model where model='printnode.release';
+                delete from ir_model where model='printnode.account';
+                delete from ir_model where model='printnode.computer';
+                delete from ir_model where model='printnode.scales';
+                delete from ir_model where model='printnode.paper';
+                delete from ir_model where model='printnode.action.method';
+                delete from ir_model where model='printnode.map.action.server';
+                delete from ir_model_fields where model='config.timbre';
+                delete from ir_model_fields where model='product.brand.ept';
+                delete from ir_model_fields where model='approval.signup';
+                delete from ir_model_fields where model='sale.call.for.price';
+                delete from ir_model_fields where model='website.stock.notify';
+                delete from ir_model_fields where model='printnode.report.policy';
+                delete from ir_model_fields where model='printnode.printer';
+                delete from ir_model_fields where model='printnode.action.button';
+                delete from ir_model_fields where model='printnode.scenario';
+                delete from ir_model_fields where model='printnode.rule';
+                delete from ir_model_fields where model='shipping.label';
+                delete from ir_model_fields where model='printnode.printjob';
+                delete from ir_model_fields where model=' printnode.release';
+                delete from ir_model_fields where model='printnode.account';
+                delete from ir_model_fields where model='printnode.computer';
+                delete from ir_model_fields where model='printnode.printer';
+                delete from ir_model_fields where model='printnode.scales';
+                delete from ir_model_fields where model='printnode.paper';
+                delete from ir_model_fields where model='printnode.action.method';
+               delete from ir_model_fields where model='printnode.map.action.server';
+               """)
     _logger.info('**********Delete******** generate_combination')
    
     _logger.info('**********Deleted******** generate_combination')
