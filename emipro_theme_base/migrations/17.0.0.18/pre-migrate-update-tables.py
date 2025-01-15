6c9fb66c64b82d6f02ba0fce8082c39fa7a95e61
Abdessamad <abdessamadbm31@gmail.com>
2025-01-15 13:34:04 +0100

    Add migration files

---
+++ emipro_theme_base/migrations/17.0.0.18/pre-migrate-update-tables.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import SUPERUSER_ID, api
+
+import logging
+_logger = logging.getLogger('Emipro:')
+
+
+def migrate(cr, version):
+    _logger.info("-------------Migration | Theme Clarico Vega | script Started---------------")
+    cr.execute("""
+    	ALTER TABLE product_template DROP COLUMN label_line_ids;
+    """)
+    _logger.info("-------------Migration | Theme Clarico Vega | script Ended ---------------")
