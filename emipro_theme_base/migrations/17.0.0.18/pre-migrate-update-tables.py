f48d4457583a0b3bf4616fe48550601dec62e6e4
Abdessamad <abdessamadbm31@gmail.com>
2025-01-15 14:10:18 +0100

    Update migration script

--- emipro_theme_base/migrations/17.0.0.18/pre-migrate-update-tables.py
+++ emipro_theme_base/migrations/17.0.0.18/pre-migrate-update-tables.py
@@ -1,39 +1,14 @@
 # -*- coding: utf-8 -*-
-# from odoo import SUPERUSER_ID, api
-
-# import logging
-# _logger = logging.getLogger('Emipro:')
-
-
-# def migrate(cr, version):
-#     _logger.info("-------------Migration | Theme Clarico Vega | script Started---------------")
-#     cr.execute("""
-#     	ALTER TABLE product_template DROP COLUMN label_line_ids;
-#     """)
-#     _logger.info("-------------Migration | Theme Clarico Vega | script Ended ---------------")
-
 
 import logging
 _logger = logging.getLogger('Emipro:')
 
 
 def migrate(cr, version):
-    _logger.info("-------------Migration | Theme Clarico Vega | Script Started---------------")
-    
-    # Check if the column exists before dropping it
+    _logger.info("-------------Migration | Theme Clarico Vega | script Started---------------")
     cr.execute("""
-        SELECT column_name 
-        FROM information_schema.columns 
-        WHERE table_name = 'product_template' AND column_name = 'label_line_ids';
+    	ALTER TABLE product_template DROP COLUMN label_line_ids;
     """)
-    column_exists = cr.fetchone()
+    _logger.info("-------------Migration | Theme Clarico Vega | script Ended ---------------")
 
-    if column_exists:
-        cr.execute("""
-            ALTER TABLE product_template DROP COLUMN label_line_ids;
-        """)
-        _logger.info("Column 'label_line_ids' dropped successfully.")
-    else:
-        _logger.info("Column 'label_line_ids' does not exist. Skipping drop operation.")
 
-    _logger.info("-------------Migration | Theme Clarico Vega | Script Ended ---------------")
